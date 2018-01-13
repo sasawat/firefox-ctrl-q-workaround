78796b2fce4cddf9cfa5ff8892534dfa5356f521
Volodymyr Kuryliak <vvkuryliak@gmail.com>
2018-01-13 15:56:23 +0200

    add Firefox Developer Edition support

--- noctrlq.sh
+++ noctrlq.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 W=`xdotool getactivewindow`                                                     
-S1=`xprop -id ${W} |awk '/WM_CLASS/{print $4}'`                                 
-if [ $S1 != '"Firefox"' ]; then
+S1=`xprop -id ${W} |awk -F '"' '/WM_CLASS/{print $4}'`                                 
+if [ "$S1" != "Firefox" ] && [ "$S1" != "Firefox Developer Edition" ]; then
 	xvkbd -xsendevent -text "\Cq"
 #	echo $S1
 fi
